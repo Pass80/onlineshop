eab5588e929190bb6301e5df4066f5ba83ef727e
Pass80 <71611559+Pass80@users.noreply.github.com>
2022-04-05 21:11:44 +0200

    deleting some files

--- src/App.js
+++ src/App.js
@@ -1,25 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+	return (
+		<div className="App">
+			<h1>Lets us build this fucking project</h1>
+			<h2>Feras is the designewr</h2>
+		</div>
+	);
+};
 
 export default App;
