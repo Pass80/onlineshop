4aa58cd0538d3ed95c939fbe82728423cf661869
Pass80 <71611559+Pass80@users.noreply.github.com>
2022-04-05 23:43:53 +0200

    creating the nav component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,26 @@
 import React from "react";
 import "./App.css";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Header from "./components/header/Header";
 
 const App = () => {
 	return (
-		<div className="App">
-			<h1>Lets us build this fucking project</h1>
-			<h2>Feras is the designewr</h2>
-		</div>
+		<Router>
+			<div className="app">
+				<Switch>
+					<Route path="/checkout">
+						<h1>this the checkoutlist</h1>
+					</Route>
+					<Route path="/login">
+						<h1>insert the required ifnos</h1>
+					</Route>
+					<Route path="/">
+						<Header />
+						<h1>Welcome to Home page</h1>
+					</Route>
+				</Switch>
+			</div>
+		</Router>
 	);
 };
 
